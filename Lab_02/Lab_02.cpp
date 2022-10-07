7721433eb028dbdbd093e60fd20a369a6bbe8c3d
Yana Nashchetska <yana.nashchetska.ri.2022@lpnu.ua>
2022-10-07 10:44:14 +0300

    first branch_02 commit

--- Lab_02/Lab_02.cpp
+++ Lab_02/Lab_02.cpp
@@ -13,17 +13,17 @@ using namespace std;
 int main()
 {
 	double x; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
-	// double z2;// результат обчислення 2-го виразу
+	// double z1; // результат обчислення 1-го виразу
+	double z2;// результат обчислення 2-го виразу
 
 	cout << "x = "; cin >> x;
 
-	z1 = (x * x + 2 * x - 3 + (x + 1) * sqrt(x * x - 9)) / (x * x - 2 * x - 3 + (x - 1) * sqrt(x * x - 9));
-	// z2 = sqrt((x + 3) / (x - 3));
+	// z1 = (x * x + 2 * x - 3 + (x + 1) * sqrt(x * x - 9)) / (x * x - 2 * x - 3 + (x - 1) * sqrt(x * x - 9));
+	z2 = sqrt((x + 3) / (x - 3));
 
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
-	// cout << "z2 = " << z2 << endl;
+	// cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
