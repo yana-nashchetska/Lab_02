b4283c485982ec8ad2ee755bac58ce9bc1133c7b
Yana Nashchetska <yana.nashchetska.ri.2022@lpnu.ua>
2022-10-07 10:25:26 +0300

    commit Lab_02

---
+++ Lab_02/Lab_02.cpp
@@ -0,0 +1,30 @@
+// Lab_2_1.cpp
+// < Нащецька Яна >
+// Лабораторна робота №2.1
+// Лінійні програми.
+// Варіант 16
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	// double z2;// результат обчислення 2-го виразу
+
+	cout << "x = "; cin >> x;
+
+	z1 = (x * x + 2 * x - 3 + (x + 1) * sqrt(x * x - 9)) / (x * x - 2 * x - 3 + (x - 1) * sqrt(x * x - 9));
+	// z2 = sqrt((x + 3) / (x - 3));
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	// cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
